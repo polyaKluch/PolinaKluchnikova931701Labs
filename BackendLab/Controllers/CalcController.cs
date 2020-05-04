e6e7e4d29cf15a73b258ed7681b0686eb5f36070
По ля <kos4v@outlook.com>
2020-05-04 16:54:49 +0700

    Finish

---
+++ BackendLab/Controllers/CalcController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BackendLab.Models;
+
+namespace BackendLab.Controllers
+{
+    public class CalcController : Controller
+    {
+        public IActionResult PassUsingModel()
+        {
+            CalcClass calc = new CalcClass();
+            return View(calc);
+        }
+        public IActionResult AccessServiceDirectly()
+        {
+            CalcClass calc = new CalcClass();
+            return View(calc);
+        }
+        public IActionResult PassUsingViewData()
+        {
+            CalcClass calc = new CalcClass();
+            ViewData["First"] = calc.First;
+            ViewData["Second"] = calc.Second;
+            ViewData["Add"] = calc.Add;
+            ViewData["Sub"] = calc.Sub;
+            ViewData["Mult"] = calc.Mult;
+            ViewData["Div"] = calc.Div;
+            return View();
+        }
+
+        public IActionResult PassUsingViewBag()
+        {
+            CalcClass calc = new CalcClass();
+            ViewBag.First = calc.First;
+            ViewBag.Second = calc.Second;
+            ViewBag.Add = calc.Add;
+            ViewBag.Sub = calc.Sub;
+            ViewBag.Mult = calc.Mult;
+            ViewBag.Div = calc.Div;
+            return View();
+        }
+    }
+}
